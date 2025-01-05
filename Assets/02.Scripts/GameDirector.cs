61ace4eaea2b01899c9bafd92b546c694c443f29
remi9023 <lch857953@gmail.com>
2025-01-05 09:32:07 +0900

    정지

--- Assets/02.Scripts/GameDirector.cs
+++ Assets/02.Scripts/GameDirector.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,8 +11,10 @@ public class GameDirector : MonoBehaviour
     public Text Playtime_Text; // 플레이 타임 텍스트
     private float delta = 0f; // 플레이 시간 기록
     public static bool isGameOver = false; // 게임 오버 상태
+    public GameObject arrowGenerator;
+    public PlayerController player;  
 
-    void Update()
+       void Update()
     {
         // 게임이 진행 중일 때만 플레이 타임 증가
         if (!isGameOver)
@@ -19,15 +23,25 @@ public class GameDirector : MonoBehaviour
             Playtime_Text.text = delta.ToString("F2") + "s";
         }
     }
+    private void Awake()
+    {
+        Time.timeScale = 1f;// 재시작 했을 때 다시 타이머가 작동하게
+        arrowGenerator.SetActive(true); // 화살도 다시 만들어지게
+    }
+
 
     public void SetHpGauge()
     {
-        // HP 감소
+        
+                // HP 감소
         hpGauge.fillAmount -= 0.1f;
 
         // 게임 오버 조건 확인
         if (hpGauge.fillAmount <= 0)
         {
+           
+            player.moveSpeed = 0f;
+            arrowGenerator?.SetActive(false);
             isGameOver = true; // 게임 오버 상태로 변경
             GameOver_Panel.SetActive(true); // 게임 오버 패널 활성화
         }
